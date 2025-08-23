97152fcca8711349bb8899c5ecc8bcdb8786aaf2
Mattia Girolimetto <mgiro2001@gmail.com>
2025-08-23 12:44:24 +0200

    feat: first commit

---
+++ filter.go
@@ -0,0 +1,34 @@
+package main
+
+import "log"
+
+type Filter interface {
+	// Return true to allow the query, false to block it
+	Filter(q string) (bool, error)
+}
+
+type TrieFilter struct {
+	root *TrieNode
+}
+
+func (f *TrieFilter) Filter(q string) (bool, error) {
+	found, err := f.root.Contains(q)
+	if err != nil {
+		return false, err
+	}
+
+	return !found, nil
+}
+
+func Trie(domains []string) Filter {
+	root := new(TrieNode)
+
+	for _, d := range domains {
+		log.Printf("INFO adding %s\n", d)
+		root.Add(d)
+	}
+
+	return &TrieFilter{
+		root: root,
+	}
+}
---
+++ trie.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"unicode"
+)
+
+const (
+	charNo = 26 + 2 // letters + "." + "-"
+	dotNo  = 26
+	dashNo = 27
+)
+
+type TrieNode struct {
+	children [charNo]*TrieNode
+}
+
+func (n *TrieNode) Add(s string) (bool, error) {
+	//log.Printf("DEBUG adding %s\n", s)
+	if s == "" {
+		// Added.
+		return true, nil
+	}
+
+	index, err := getIndex(rune(s[0]))
+	if err != nil {
+		return false, fmt.Errorf("trie: add: %w", err)
+	}
+
+	if n.children[index] == nil {
+		n.children[index] = new(TrieNode)
+	}
+
+	return n.children[index].Add(s[1:])
+}
+
+func (n *TrieNode) Contains(s string) (bool, error) {
+	//log.Printf("DEBUG looking for %s\n", s)
+	if s == "" {
+		return true, nil
+	}
+
+	index, err := getIndex(rune(s[0]))
+	if err != nil {
+		return false, fmt.Errorf("trie: add: %w", err)
+	}
+
+	if n.children[index] == nil {
+		return false, nil
+	}
+
+	return n.children[index].Contains(s[1:])
+}
+
+func getIndex(r rune) (int, error) {
+	if !unicode.IsLetter(r) {
+		switch r {
+		case '.':
+			return dotNo, nil
+		case '-':
+			return dashNo, nil
+		default:
+			return -1, fmt.Errorf("invalid rune '%s'", r)
+		}
+	}
+
+	upper := unicode.ToUpper(r)
+	return int(upper - 'A'), nil
+}
