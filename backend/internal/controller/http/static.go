b3a119cec31fdd5083da8768e36528b979edafde
mattia <mgiro2001@gmail.com>
2026-02-28 20:19:40 +0100

    fix: frontend routes

--- backend/internal/controller/http/static.go
+++ backend/internal/controller/http/static.go
@@ -10,8 +10,19 @@ import (
 
 func serveStatic(r *chi.Mux) {
 	workDir, _ := os.Getwd()
-	feDir := http.Dir(filepath.Join(workDir, "frontend"))
+	feDir := filepath.Join(workDir, "frontend")
 
-	fs := http.StripPrefix("/", http.FileServer(feDir))
-	r.Handle("/*", fs)
+	r.Handle("/*", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		path := filepath.Join(feDir, r.URL.Path)
+
+		// Check if the file exists
+		_, err := os.Stat(path)
+		if os.IsNotExist(err) {
+			// Fall back to index.html for client-side routing
+			http.ServeFile(w, r, filepath.Join(feDir, "index.html"))
+			return
+		}
+
+		http.FileServer(http.Dir(feDir)).ServeHTTP(w, r)
+	}))
 }
